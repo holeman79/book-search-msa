87dd95e0982935d43f51c1b70b42ab8c6c77d8af
holeman79 <holeman79@gmail.com>
2020-07-07 23:40:07 +0900

    feign 삭제

--- book/src/main/java/com/kakao/book/book/service/impl/BookServiceImpl.java
+++ book/src/main/java/com/kakao/book/book/service/impl/BookServiceImpl.java
@@ -2,9 +2,7 @@ package com.kakao.book.book.service.impl;
 
 import com.kakao.book.book.domain.dto.BookSearchRequest;
 import com.kakao.book.book.domain.dto.BookSearchResponse;
-import com.kakao.book.book.domain.kakao.KakaoBooks;
 import com.kakao.book.book.service.BookService;
-import com.kakao.book.book.service.FeignKakaoBookRemoteService;
 import com.kakao.book.book.service.remote.KakaoBookRemoteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,8 +14,6 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class BookServiceImpl implements BookService {
     private final KakaoBookRemoteService kakaoBookRemoteService;
-
-    private final FeignKakaoBookRemoteService feignKakaoBookRemoteService;
 
     @Override
     public Mono<BookSearchResponse> getBooksByExternalApi(BookSearchRequest bookSearchRequest) {
